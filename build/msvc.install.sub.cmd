3f6829a33d1d2ce3d3c644581912d72597adf329
Grigore Stefan <g_stefan@yahoo.com>
2021-11-29 14:47:47 +0200

    Update

--- build/msvc.install.sub.cmd
+++ build/msvc.install.sub.cmd
@@ -11,8 +11,8 @@ if errorlevel 1 exit 1
 rem ---
 
 if not exist "%INSTALL_PATH_BIN%\" mkdir "%INSTALL_PATH_BIN%"
-xcopy /Y /S /E "temp\expat\bin\*.exe" "%INSTALL_PATH_BIN%\"
-xcopy /Y /S /E "temp\expat\bin\*.dll" "%INSTALL_PATH_BIN%\"
+xcopy /Y /S /E "output\bin\*.exe" "%INSTALL_PATH_BIN%\"
+xcopy /Y /S /E "output\bin\*.dll" "%INSTALL_PATH_BIN%\"
 
 rem --- dev
 
@@ -20,9 +20,9 @@ if not exist "%INSTALL_PATH_DEV%\" mkdir "%INSTALL_PATH_DEV%"
 if not exist "%INSTALL_PATH_DEV%\bin" mkdir "%INSTALL_PATH_DEV%\bin"
 if not exist "%INSTALL_PATH_DEV%\include" mkdir "%INSTALL_PATH_DEV%\include"
 if not exist "%INSTALL_PATH_DEV%\lib" mkdir "%INSTALL_PATH_DEV%\lib"
-xcopy /Y /S /E "temp\expat\bin\*.exe" "%INSTALL_PATH_DEV%\bin\"
-xcopy /Y /S /E "temp\expat\bin\*.dll" "%INSTALL_PATH_DEV%\bin\"
-xcopy /Y /S /E "temp\expat\include\" "%INSTALL_PATH_DEV%\include\"
-copy /Y /B "temp\expat\lib\*.lib" "%INSTALL_PATH_DEV%\lib\*"
+xcopy /Y /S /E "output\bin\*.exe" "%INSTALL_PATH_DEV%\bin\"
+xcopy /Y /S /E "output\bin\*.dll" "%INSTALL_PATH_DEV%\bin\"
+xcopy /Y /S /E "output\include\" "%INSTALL_PATH_DEV%\include\"
+copy /Y /B "output\lib\*.lib" "%INSTALL_PATH_DEV%\lib\*"
 
-copy /Y /B "temp\expat\lib\libexpat.lib" "%INSTALL_PATH_DEV%\lib\expat.lib"
+copy /Y /B "output\lib\libexpat.lib" "%INSTALL_PATH_DEV%\lib\expat.lib"
