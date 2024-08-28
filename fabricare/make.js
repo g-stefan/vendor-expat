8de975c3e7f8ac7e5417e594ed6853173935eb51
Grigore Stefan <g_stefan@yahoo.com>
2024-08-28 17:06:55 +0300

    Update

--- fabricare/make.js
+++ fabricare/make.js
@@ -25,29 +25,41 @@ Shell.mkdirRecursivelyIfNotExists("temp");
 Shell.mkdirRecursivelyIfNotExists("temp/cmake");
 
 if (!Shell.fileExists("temp/build.config.flag")) {
-	Shell.copyFile("fabricare/CMakeLists.txt","source/CMakeLists.txt");
+	Shell.setenv("CC", "cl.exe");
+	Shell.setenv("CXX", "cl.exe");
 
-	Shell.setenv("CC","cl.exe");
-	Shell.setenv("CXX","cl.exe");
+	cmdConfig = "cmake";
+	cmdConfig += " ../../source";
+	cmdConfig += " -G \"Ninja\"";
+	cmdConfig += " -DCMAKE_BUILD_TYPE=Release";
+	cmdConfig += " -DCMAKE_INSTALL_PREFIX=" + Shell.realPath(Shell.getcwd()) + "\\output";
 
-	cmdConfig="cmake";
-	cmdConfig+=" ../../source";
-	cmdConfig+=" -G \"Ninja\"";
-	cmdConfig+=" -DCMAKE_BUILD_TYPE=Release";
-	cmdConfig+=" -DCMAKE_INSTALL_PREFIX="+Shell.realPath(Shell.getcwd())+"\\output";
+	if (Fabricare.isStatic()) {
+		cmdConfig += " -DBUILD_SHARED_LIBS=OFF";
+		cmdConfig += " -DEXPAT_MSVC_STATIC_CRT=ON";
+		cmdConfig += " -DEXPAT_SHARED_LIBS=OFF";
+	};
 
-	runInPath("temp/cmake",function(){
+	runInPath("temp/cmake", function () {
 		exitIf(Shell.system(cmdConfig));
 	});
 
 	Shell.filePutContents("temp/build.config.flag", "done");
 };
 
-runInPath("temp/cmake",function(){
+if (Fabricare.isStatic()) {
+	Shell.copyFile("fabricare/source/expat_external.h", "source/lib/expat_external.h");
+};
+
+runInPath("temp/cmake", function () {
 	exitIf(Shell.system("ninja"));
 	exitIf(Shell.system("ninja install"));
 	exitIf(Shell.system("ninja clean"));
 });
+
+if(OS.isWindows()){
+	Shell.copyFile("output/lib/libexpatMT.lib","output/lib/libexpat.lib");
+};
 
 Shell.filePutContents("temp/build.done.flag", "done");
 
